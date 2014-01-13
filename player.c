3289532c6ffd9ce90a186b84f80132e0c3491316
Ievgenii Baliuk <i.baliuk@samsung.com>
2014-01-13 18:26:44 +0200

    user friendly tags

--- player.c
+++ player.c
@@ -64,7 +64,32 @@ static gboolean cb_print_position(GstElement *pipeline)
   return TRUE;
 }
 
-static void print_one_tag (const GstTagList * list, const gchar * tag, gpointer user_data)
+static void print_one_tag_line(const gchar *tag, const GValue *val)
+{
+  if (G_VALUE_HOLDS_STRING (val)) {
+    g_print ("\t%10s : %s\n", tag, g_value_get_string (val));
+  } else if (G_VALUE_HOLDS_UINT (val)) {
+    g_print ("\t%10s : %u\n", tag, g_value_get_uint (val));
+  } else if (G_VALUE_HOLDS_DOUBLE (val)) {
+    g_print ("\t%10s : %g\n", tag, g_value_get_double (val));
+  } else if (G_VALUE_HOLDS_BOOLEAN (val)) {
+    g_print ("\t%10s : %s\n", tag,
+        (g_value_get_boolean (val)) ? "true" : "false");
+  } else if (GST_VALUE_HOLDS_BUFFER (val)) {
+    GstBuffer *buf = gst_value_get_buffer (val);
+    guint buffer_size = gst_buffer_get_size (buf);
+    g_print ("\t%10s : buffer of size %u\n", tag, buffer_size);
+  } else if (GST_VALUE_HOLDS_DATE_TIME (val)) {
+    GstDateTime *dt = g_value_get_boxed (val);
+    gchar *dt_str = gst_date_time_to_iso8601_string (dt);
+    g_print ("\t%10s : %s\n", tag, dt_str);
+    g_free (dt_str);
+  } else {
+    g_print ("\t%10s : tag of type ’%s’\n", tag, G_VALUE_TYPE_NAME (val));
+  }
+}
+
+static void print_one_tag (const GstTagList *list, const gchar *tag, gpointer user_data)
 {
   int i, num;
   num = gst_tag_list_get_tag_size (list, tag);
@@ -73,27 +98,16 @@ static void print_one_tag (const GstTagList * list, const gchar * tag, gpointer 
     /* Note: when looking for specific tags, use the gst_tag_list_get_xyz() API,
      * * we only use the GValue approach here because it is more generic */
     val = gst_tag_list_get_value_index (list, tag, i);
-    if (G_VALUE_HOLDS_STRING (val)) {
-      g_print ("\t%20s : %s\n", tag, g_value_get_string (val));
-    } else if (G_VALUE_HOLDS_UINT (val)) {
-      g_print ("\t%20s : %u\n", tag, g_value_get_uint (val));
-    } else if (G_VALUE_HOLDS_DOUBLE (val)) {
-      g_print ("\t%20s : %g\n", tag, g_value_get_double (val));
-    } else if (G_VALUE_HOLDS_BOOLEAN (val)) {
-      g_print ("\t%20s : %s\n", tag,
-          (g_value_get_boolean (val)) ? "true" : "false");
-    } else if (GST_VALUE_HOLDS_BUFFER (val)) {
-      GstBuffer *buf = gst_value_get_buffer (val);
-      guint buffer_size = gst_buffer_get_size (buf);
-      g_print ("\t%20s : buffer of size %u\n", tag, buffer_size);
-    } else if (GST_VALUE_HOLDS_DATE_TIME (val)) {
-      GstDateTime *dt = g_value_get_boxed (val);
-      gchar *dt_str = gst_date_time_to_iso8601_string (dt);
-      g_print ("\t%20s : %s\n", tag, dt_str);
-      g_free (dt_str);
-    } else {
-      g_print ("\t%20s : tag of type ’%s’\n", tag, G_VALUE_TYPE_NAME (val));
-    }
+    if(g_strrstr(tag, "artist"))
+      print_one_tag_line("Artist", val);
+    else if(g_strrstr(tag, "title"))
+      print_one_tag_line("Song", val);
+    else if(g_strrstr(tag, "genre"))
+      print_one_tag_line("Genre", val);
+    else if(g_strrstr(tag, "datetime"))
+      print_one_tag_line("Year", val);
+    else if(g_strrstr(tag, "album"))
+      print_one_tag_line("Album", val);
   }
 }
 
@@ -196,6 +210,7 @@ gint main(gint argc, gchar *argv[])
   GstTagList *tags = NULL;
   msg = gst_bus_timed_pop_filtered (GST_ELEMENT_BUS (player->pipeline), GST_CLOCK_TIME_NONE, GST_MESSAGE_TAG);
   gst_message_parse_tag (msg, &tags);
+  g_print ("\n");
   gst_tag_list_foreach (tags, print_one_tag, NULL);
   g_print ("\n");
   gst_tag_list_unref (tags);
